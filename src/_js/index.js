31d42ff3bd34eff7c563334598ec86b8dd989563
flickflack3007 <flickflack3007@gmx.de>
2019-10-11 16:32:06 +0200

    integration parceljs

---
+++ src/_js/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/*import stylesheet from "../_css/index.css";*/
+import App from "./app.js";
+
+
+window.addEventListener("load", () => 
+{
+    let app = new App();
+    app.start();
+});

38c46844801f4cf2810bf6c50c9b0ce0c4ab1f9a
flickflack3007 <flickflack3007@gmx.de>
2019-10-17 11:05:43 +0200

    projektstruktur > home, login, createNotice

---
+++ src/login/login.js
@@ -0,0 +1,15 @@
+"use strict"
+
+import stylesheet from "../_css/login.css";
+
+class Login {
+
+    constructor() {
+    }
+
+    startLogin()
+    {
+        window.console.log("Login");
+    }
+}
+export default Login;
